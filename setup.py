bb326ac5fd85bede86e06ad122a521ab7970f3cf
Matthijs van Eede <matthijsvaneede@gmail.com>
2017-03-06 10:25:30 -0500

    added empty setup.py. With an example line that can be used to add scripts to this repository

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import sys
+
+setup(name="MICe-lab",
+      version='0.1',
+      #install_requires=['numpy', 'scipy', 'pyminc'],
+      scripts=[
+               #"python/example_script.py",
+               ],
+      )
