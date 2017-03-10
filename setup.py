f665d820d6cebb16448f2a8c05a04fba95ca64b2
edeguzman <aesdeguzman@gmail.com>
2017-03-10 14:09:48 -0500

    Add install information to setup.py for saddle_recon code

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ import sys
 setup(name="MICe-lab",
       version='0.1',
       #install_requires=['numpy', 'scipy', 'pyminc'],
-      scripts=[
-               #"python/example_script.py",
-               ],
+      install_requires=[
+        'ConfigArgParse>=0.11',
+        'numpy',
+        'pyminc',
+        'pydpiper',
+        'typing',
+      ],
+      scripts=['python/saddle_recon/saddle_recon.py'],
       )
