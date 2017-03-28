ed6d80df3f3e9889bf22bb346cc6674b787b351d
Ben Darwin <bcdarwin@gmail.com>
2017-03-28 14:33:16 -0400

    add CT embryo cropping

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ import sys
 
 setup(name="MICe-lab",
       version='0.1',
-      #install_requires=['numpy', 'scipy', 'pyminc'],
+      install_requires=['snakemake'], #'numpy', 'scipy', 'pyminc'],
       scripts=[
                "python/convert_CT_image.py",
+               "python/Snakefile"
                ],
       )
