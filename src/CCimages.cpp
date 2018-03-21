c5d56816a0106cb48cf217621561c5b17bd0a2c4
nzxwang <nickywang100@gmail.com>
2018-03-21 12:30:54 -0400

    can compile CCimages.cpp

--- src/CCimages.cpp
+++ src/CCimages.cpp
@@ -1,10 +1,10 @@
 #include <opencv2/opencv.hpp>
-
+#include <getopt.h>
 using namespace cv;
 using namespace std;
 
 //compile with: 
-// g++ CCimages.cpp -o CCimages -L/usr/local/lib -I/usr/local/include/opencv -L${exec_prefix}/lib -lopencv_shape -lopencv_stitching -lopencv_objdetect -lopencv_superres -lopencv_videostab -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_video -lopencv_photo -lopencv_ml -lopencv_imgproc -lopencv_flann -lopencv_viz -lopencv_core
+// g++ -o CCimages CCimages.cpp -L/axiom2/projects/software/arch/linux-xenial-xerus/opencv/3.1.0/share/OpenCV/3rdparty/lib `pkg-config --cflags --libs opencv
 
 int search_cols=-1,search_rows=-1;
 int refx0,refy0,refx1,refy1;
