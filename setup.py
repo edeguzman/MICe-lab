064a7742f57b5653653ebb1900a4d7924d36eda1
nzxwang <nickywang100@gmail.com>
2018-03-26 11:25:19 -0400

    added commas where needed

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ import sys
 
 setup(name="MICe-lab",
       version='0.18',
-      description='Scripts and pipelines that are specific to the Mouse Imaging Centre lab.'
-      long_description='Scripts and pipelines that are specific to the Mouse Imaging Centre lab.'
-      url='https://github.com/Mouse-Imaging-Centre/MICe-lab'
+      description='Scripts and pipelines that are specific to the Mouse Imaging Centre lab.',
+      long_description='Scripts and pipelines that are specific to the Mouse Imaging Centre lab.',
+      url='https://github.com/Mouse-Imaging-Centre/MICe-lab',
       install_requires=[
         'pydpiper>=2.0.9',
         'ConfigArgParse>=0.11',
@@ -19,7 +19,7 @@ setup(name="MICe-lab",
         'snakemake',
         'matplotlib'
       ],
-      dependency_links=['https://github.com/opencv/opencv/archive/3.1.0.tar.gz']
+      dependency_links=['https://github.com/opencv/opencv/archive/3.1.0.tar.gz'],
       packages=['python', 'python.saddle_recon', 'python.mri_python'],
       scripts=['python/saddle_recon/saddle_recon.py',
                'python/convert_CT_image.py',
