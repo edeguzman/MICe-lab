7cee0c2bfde11e17477a2161b7159b8b22a86f9b
nzxwang <nickywang100@gmail.com>
2018-04-27 11:28:53 -0400

    cellprofiler.py is a standalone pipeline

--- python/tissvis/cellprofiler.py
+++ python/tissvis/cellprofiler.py
@@ -4,7 +4,6 @@ import os
 
 from configargparse import ArgParser, Namespace
 from pydpiper.core.stages import Stages, Result, CmdStage
-from pydpiper.core.arguments import AnnotatedParser, BaseParser, CompoundParser
 from pydpiper.execution.application import mk_application
 
 from tissvis.arguments import cellprofiler_parser
@@ -27,33 +26,15 @@ def cellprofiler_pipeline(options):
     pipeline_name = options.application.pipeline_name
     s = Stages()
 
-    #############################
-    # Step 1: Run cellprofiler.py
-    #############################
     env_vars={}
-    env_vars['PYTHONPATH']=options.cellprofiler.cellprofiler.python2_path
+    env_vars['PYTHONPATH']=options.cellprofiler.python2_path
 
     cellprofiler_results = s.defer(cellprofiler_batch(application_options=options.application, \
-            cellprofiler_options=options.cellprofiler.cellprofiler, env_vars = env_vars, output_dir=output_dir))
+            cellprofiler_options=options.cellprofiler, env_vars = env_vars, output_dir=output_dir))
 
     return Result(stages=s, output=Namespace(cellprofiler_output=cellprofiler_results, ))
 
-
-#############################
-# Make Parser & Application
-#############################
-
-#cellprofiler_parser = CompoundParser([cellprofiler_parser])
-
-#cellprofiler_application = mk_application(
-    #parsers=[AnnotatedParser(parser=cellprofiler_parser, namespace='cellprofiler')],
-    #pipeline=cellprofiler_pipeline)
-
 cellprofiler_application = mk_application(parsers=[cellprofiler_parser], pipeline=cellprofiler_pipeline)
-#lsq6_application = mk_application(parsers=[lsq6_parser], pipeline=lsq6_pipeline)
-#############################
-# Run
-#############################
 
 if __name__ == "__main__":
     cellprofiler_application()
