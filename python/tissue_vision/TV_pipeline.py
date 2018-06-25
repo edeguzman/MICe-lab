fb7622f2ff765c58a8b50dd9647aaa4a80a6e70a
nzxwang <nickywang100@gmail.com>
2018-06-25 11:16:50 -0400

    fix output again

--- python/tissue_vision/TV_pipeline.py
+++ python/tissue_vision/TV_pipeline.py
@@ -99,9 +99,10 @@ def tissue_vision_pipeline(options):
             all_TV_stitch_results.append(TV_stitch_result)
 
         if brain.z_section:
+            import pdb; pdb.set_trace()
             TV_stitch_result = s.defer(TV_stitch_wrap(brain_directory=brain.brain_directory,
                                                       brain_name=brain.name,
-                                                      stitched=stitched[brain.z_start:brain.z_section - 1],
+                                                      stitched=stitched[brain.z_start - 1 : brain.z_section - 1],
                                                       TV_stitch_options=options.tissue_vision.TV_stitch,
                                                       Zstart=brain.z_start,
                                                       Zend=brain.z_section - 1,
